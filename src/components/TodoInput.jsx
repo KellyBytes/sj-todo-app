2568715be3e532bd2a7953893636e612a08073d8
eiwashita <erikoiwashita@gmail.com>
2025-09-11 15:43:08 -0600

    Add Calendar icon

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,9 +1,16 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 const TodoInput = (props) => {
   const { handleAddTodo } = props;
   const [inputValue, setInputValue] = useState('');
   const [dueDate, setDueDate] = useState('');
+  const hiddenInputRef = useRef(null);
+
+  const handleOpenDatePicker = () => {
+    if (hiddenInputRef.current) {
+      hiddenInputRef.current.showPicker();
+    }
+  };
 
   return (
     // <div className="input-container">
@@ -26,9 +33,13 @@ const TodoInput = (props) => {
         onChange={(e) => setInputValue(e.target.value)}
         placeholder="Add task"
       />
+      <button type="button" onClick={handleOpenDatePicker}>
+        <i className="fa-regular fa-calendar"></i>
+      </button>
       <input
         type="date"
-        value={dueDate}
+        ref={hiddenInputRef}
+        style={{ display: 'none' }}
         onChange={(e) => setDueDate(e.target.value)}
       />
       <button type="submit">
